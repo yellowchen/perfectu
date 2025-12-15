4e1658dd3e2a76af855ae667ab55dbece14c425a
yellow <yellowchen1103@gmail.com>
2025-12-15 19:18:49 +0800

    revise formatDate

--- src/Common/utils/dateUtils/date-utils.jsx
+++ src/Common/utils/dateUtils/date-utils.jsx
@@ -12,11 +12,7 @@ export const FormatDate = (date) => {
 };
 
 export const FormatDateTimesK = (date) => {
-<<<<<<< HEAD
 	return `${new Date(date * 1000).getFullYear().toString()}-${dateAddZero(
-=======
-	return `${new Date(date * 1000).getFullYear().toString()} - ${dateAddZero(
->>>>>>> fa0893d2817b34e3143d9e09954c0a8f74d06685
 		new Date(date * 1000).getMonth() + 1
 	)}-${dateAddZero(new Date(date * 1000).getDate())}`;
 };
